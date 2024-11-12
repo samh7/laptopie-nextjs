d3d51e2638f53e4163a3cf22a3ff691fb390a324
samh7 <samh74490@example.com>
2024-11-12 16:55:09 +0300

    Add get recommendations for image

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -2,7 +2,7 @@
 
 import { QuizAnswers } from "@/interfaces/interfaces";
 
-export async function getRecommendations(data: QuizAnswers) {
+export async function getRecommendations(data: QuizAnswers | string) {
   "use server";
 
   console.log(JSON.stringify(data));
@@ -18,11 +18,41 @@ export async function getRecommendations(data: QuizAnswers) {
   return responseData;
 }
 
+export async function getRecommendationsImage(data: File | FormDataEntryValue | FormData) {
+  "use server";
+
+  // console.log(JSON.stringify(data));
+  const res = await fetch(`${process.env.LAPTOPIE_URL}/api/reccomendImage`, {
+    method: "POST",
+    body: data,
+  });
+
+  const responseData = await res.json();
+  return responseData;
+}
+
 export async function chatWithLaptop(data: string) {
   "use server";
 
   console.log(JSON.stringify(data));
   const res = await fetch(`${process.env.LAPTOPIE_URL}/api/chatwithlaptop`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  // Parse the response data before returning
+  const responseData = await res.json();
+  return responseData;
+}
+
+export async function getRecommendationsFree(data: string) {
+  "use server";
+
+  console.log(JSON.stringify(data));
+  const res = await fetch(`${process.env.LAPTOPIE_URL}/api/reccomendfree`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
