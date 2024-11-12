4926d62c6e23292986ade0bf6dec236662ce4d26
samh7 <samh74490@example.com>
2024-11-12 21:51:26 +0300

    Move to lib

--- src/lib/interfaces/interfaces.ts
+++ src/lib/interfaces/interfaces.ts
@@ -1,4 +1,39 @@
+import { IconType } from "react-icons/lib";
+
 interface AnswerOption {
+  title: string;
+  description: string;
+}
+export interface Specification {
+  title: string;
+  description: string;
+  detailedExplanation: string;
+  recommendation: string;
+  externalLinks: {
+    text: string;
+    url: string;
+  }[];
+  category: string;
+  cheatsheet?: {
+    prefix: string;
+    meaning: string;
+  }[];
+}
+
+export interface ChatMessage {
+  id: string;
+  type: "user" | "assistant";
+  content: string;
+  timestamp: Date;
+}
+
+export interface FAQItem {
+  question: string;
+  answer: string;
+}
+
+export interface UseCase {
+  icon: IconType;
   title: string;
   description: string;
 }
@@ -14,7 +49,7 @@ export interface QuizAnswers {
   storage?: AnswerOption[];
 }
 
-export  interface LaptopSpecs {
+export interface LaptopSpecs {
   cpu: string;
   gpu: string;
   ram: number;
@@ -38,9 +73,6 @@ export interface Recommnedation {
   recommendations: Laptop[];
 }
 
-
-
-
 export type QuizStep =
   | "usage"
   | "workload"
@@ -51,7 +83,7 @@ export type QuizStep =
   | "storage"
   | "priority";
 
-  export  interface QuizState {
+export interface QuizState {
   currentStep: QuizStep;
   selections: {
     usage: string[];
