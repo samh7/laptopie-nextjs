093f33c746c660d06b9d8aa0b2611e57251f148a
samh7 <samh74490@example.com>
2024-11-14 03:59:43 +0300

    ignore

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,8 +55,48 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			},
+  			marquee: {
+  				'0%': { transform: 'translateX(0)' },
+  				'100%': { transform: 'translateX(-100%)' },
+  			},
+  			'marquee-reverse': {
+  				'0%': { transform: 'translateX(0)' },
+  				'100%': { transform: 'translateX(100%)' },
+  			},
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			marquee: 'marquee 25s linear infinite',
+  			'marquee-reverse': 'marquee-reverse 25s linear infinite',
+  		},
+  		utilities: {
+  			'.pause': {
+  				'animation-play-state': 'paused',
+  			},
+  		},
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('tailwind-scrollbar-hide')
+  ],
 } satisfies Config;
