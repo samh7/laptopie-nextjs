5d1a96813d282c17b58fc647ce6b794a5bc383c8
samh7 <samh74490@example.com>
2024-11-22 06:59:42 +0300

    center cons and pros card and remove border

--- src/components/info/ProsAndCons.tsx
+++ src/components/info/ProsAndCons.tsx
@@ -9,50 +9,52 @@ export default function ProsAndCons({
   reccomendedLaptop: Laptop;
 }) {
   return (
-    <div className="grid md:grid-cols-2 gap-6 mb-16">
-      <Card>
-        <CardHeader>
-          <CardTitle className="flex items-center gap-2 text-emerald-600">
-            <FaCheckCircle className="h-5 w-5" />
-            Advantages
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <ul className="space-y-3">
-            {reccomendedLaptop?.pros?.map((pro: string, index: number) => (
-              <li
-                key={index}
-                className="flex items-center gap-3 text-muted-foreground hover:text-foreground transition-colors"
-              >
-                <div className="w-1.5 h-1.5 rounded-full bg-emerald-600" />
-                {pro}
-              </li>
-            )) || []}
-          </ul>
-        </CardContent>
-      </Card>
+    <div className="w-full flex flex-col items-center md:flex-row md:justify-center  gap-6 mb-16">
+      <div className="md:flex">
+        <Card className="border-none">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-2 text-emerald-600">
+              <FaCheckCircle className="h-5 w-5" />
+              Advantages
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ul className="space-y-3">
+              {reccomendedLaptop?.pros?.map((pro: string, index: number) => (
+                <li
+                  key={index}
+                  className="flex items-center gap-3 text-muted-foreground hover:text-foreground transition-colors"
+                >
+                  <div className="w-1.5 h-1.5 rounded-full bg-emerald-600" />
+                  {pro}
+                </li>
+              )) || []}
+            </ul>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader>
-          <CardTitle className="flex items-center gap-2 text-destructive">
-            <FaTimesCircle className="h-5 w-5" />
-            Limitations
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <ul className="space-y-3">
-            {reccomendedLaptop?.cons?.map((con: string, index: number) => (
-              <li
-                key={index}
-                className="flex items-center gap-3 text-muted-foreground hover:text-foreground transition-colors"
-              >
-                <div className="w-1.5 h-1.5 rounded-full bg-destructive" />
-                {con}
-              </li>
-            )) || []}
-          </ul>
-        </CardContent>
-      </Card>
+        <Card className="border-none">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-2 text-destructive">
+              <FaTimesCircle className="h-5 w-5" />
+              Limitations
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ul className="space-y-3">
+              {reccomendedLaptop?.cons?.map((con: string, index: number) => (
+                <li
+                  key={index}
+                  className="flex items-center gap-3 text-muted-foreground hover:text-foreground transition-colors"
+                >
+                  <div className="w-1.5 h-1.5 rounded-full bg-destructive" />
+                  {con}
+                </li>
+              )) || []}
+            </ul>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
